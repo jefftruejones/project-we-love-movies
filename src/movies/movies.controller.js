b8a2f45bcc52f796f10bc3f8986cb4ca46d5f12d
Jeff Jones <jeffreydonovanjones@gmail.com>
2021-05-19 11:50:53 -0400

    inital commit

---
+++ src/movies/movies.controller.js
@@ -0,0 +1,26 @@
+const service = require("./movies.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+async function list(req, res, next) {
+  const data = await service.list(req.query.is_showing);
+  res.json({ data });
+}
+
+async function read(req, res, next) {
+  const { movieId } = req.params;
+  const response = await service.read(movieId);
+  if (response.length === 0) {
+    next({ status: 404, message: "Movie cannot be found." });
+  } else {
+    const reply = "yes";
+
+    // console.log("response is", response);
+
+    res.json({ data: response[0] });
+  }
+}
+
+module.exports = {
+  list: asyncErrorBoundary(list),
+  read: asyncErrorBoundary(read),
+};
